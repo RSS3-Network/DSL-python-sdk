f8f5b22536abb82afb38f41614c688d86e6cd39d
Thomas <wxy_000000@qq.com>
2024-07-16 15:38:33 +0800

    chore: Add various enums for Direction, Network, Platform, ActivityTag, and ActivityType

---
+++ rss3_dsl_client/enums.py
@@ -0,0 +1,81 @@
+from enum import Enum
+
+class Direction(str, Enum):
+    IN = 'in'
+    OUT = 'out'
+    SELF = 'self'
+
+
+class Network(str, Enum):
+    ARBITRUM = 'arbitrum'
+    ARWEAVE = 'arweave'
+    AVAX = 'avax'
+    BASE = 'base'
+    BINANCE_SMART_CHAIN = 'binance-smart-chain'
+    CROSSBELL = 'crossbell'
+    ETHEREUM = 'ethereum'
+    FARCASTER = 'farcaster'
+    GNOSIS = 'gnosis'
+    LINEA = 'linea'
+    OPTIMISM = 'optimism'
+    POLYGON = 'polygon'
+    VSL = 'vsl'
+
+
+class Platform(str, Enum):
+    INCH = '1inch'
+    AAVE = 'AAVE'
+    AAVEGOTCHI = 'Aavegotchi'
+    CROSSBELL = 'Crossbell'
+    CURVE = 'Curve'
+    ENS = 'ENS'
+    FARCASTER = 'Farcaster'
+    HIGHLIGHT = 'Highlight'
+    IQWIKI = 'IQWiki'
+    KIWISTAND = 'KiwiStand'
+    LENS = 'Lens'
+    LIDO = 'Lido'
+    LOOKSRARE = 'LooksRare'
+    MATTERS = 'Matters'
+    MIRROR = 'Mirror'
+    OPENSEA = 'OpenSea'
+    OPTIMISM = 'Optimism'
+    PARAGRAPH = 'Paragraph'
+    RSS3 = 'RSS3'
+    SAVM = 'SAVM'
+    STARGATE = 'Stargate'
+    UNISWAP = 'Uniswap'
+    UNKNOWN = 'Unknown'
+    VSL = 'VSL'
+
+
+class ActivityTag(str, Enum):
+    COLLECTIBLE = 'collectible'
+    EXCHANGE = 'exchange'
+    METAVERSE = 'metaverse'
+    RSS = 'rss'
+    SOCIAL = 'social'
+    TRANSACTION = 'transaction'
+    UNKNOWN = 'unknown'
+
+
+class ActivityType(str, Enum):
+    APPROVAL = 'approval'
+    BRIDGE = 'bridge'
+    BURN = 'burn'
+    COMMENT = 'comment'
+    DELETE = 'delete'
+    FEED = 'feed'
+    LIQUIDITY = 'liquidity'
+    MINT = 'mint'
+    POST = 'post'
+    PROFILE = 'profile'
+    PROXY = 'proxy'
+    REVISE = 'revise'
+    REWARD = 'reward'
+    SHARE = 'share'
+    STAKING = 'staking'
+    SWAP = 'swap'
+    TRADE = 'trade'
+    TRANSFER = 'transfer'
+    UNKNOWN = 'unknown'
